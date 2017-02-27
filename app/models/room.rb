fbcaa64fc7c61acb54c2986aa7c87be015618a13
Giancarlo Malugani <monkodev@gmail.com>
2017-02-26 23:08:54 -0300

    Agrega validaciones a rooms

--- app/models/room.rb
+++ app/models/room.rb
@@ -13,4 +13,7 @@
 #
 
 class Room < ActiveRecord::Base
+	validates_presence_of :title, :description, :beds, :guests, :image_url
+	validates_length_of :description, :maximum => 400, :allow_blank => false
+	validates :beds, :guests, numericality: { only_integer: true, :greater_than_or_equal_to => 1}
 end
