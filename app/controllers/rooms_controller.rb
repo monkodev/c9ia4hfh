886511a545f0d0d1e6bc73297e66f317355e3bf1
Giancarlo Malugani <monkodev@gmail.com>
2017-02-27 17:01:15 -0300

    Implementa edicion/eliminacion rooms

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -16,6 +16,25 @@ class RoomsController < ApplicationController
     end
   end
 
+  def edit
+    @room = Room.find(params[:id])
+  end
+
+  def update
+    @room = Room.find(params[:id])
+    if @room.update(room_params)
+      redirect_to rooms_path, notice: "La habitación ha sido modificada"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    room = Room.find(params[:id])
+    room.destroy
+    redirect_to rooms_path
+  end
+
   protected
     def room_params
       params.require(:room).permit(:title, :description, :beds, :guests, :image_url,:price_per_night)
